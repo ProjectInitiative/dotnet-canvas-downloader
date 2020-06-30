df101428fdac7a385d0d53ca79c0ef88dddb2c1d
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-06-29 23:54:28 -0500

    Added basic commandline argument port
    - Ported over handling command line features from python.
    - Created basic checks for directory validation and other user checks

--- CanvasDownloader.cs
+++ CanvasDownloader.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using CommandLine;
 
 
@@ -6,22 +9,66 @@ namespace canvas_downloader
 {
     class CanvasDownloader
     {
+        static Options opts { get; set; }
+        private static string appRootPath { get; } = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        static string rootPath { get; set; } = CombinePaths(Path.GetFullPath(Path.GetDirectoryName(appRootPath)), "courses");
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args)
-                    .WithParsed<Options>(o =>
+            //parse command line arguments
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+            if (opts != null)
+            {
+                if (opts.NoFiles && opts.NoModules)
+                {
+                    return;
+                }
+
+                //check if the provided filepath if valid
+                if (opts.Output != null)
+                {
+                    try
                     {
-                        if (o.Verbose)
-                        {
-                            Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
-                            Console.WriteLine("Quick Start Example! App is in Verbose mode!");
-                        }
-                        else
-                       {
-                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
-                           Console.WriteLine("Quick Start Example!");
-                       }
-                    });
+                        rootPath = CombinePaths(Path.GetFullPath(opts.Output), "courses");
+                        Directory.CreateDirectory(rootPath);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Invalid file path. Please enter a valid path.");
+                        return;
+                    }
+                }
+            }
+        }
+
+        static string CombinePaths(params string[] paths)
+        {
+            if (paths == null)
+            {
+                throw new ArgumentNullException("paths");
+            }
+            return paths.Aggregate(Path.Combine);
+        }
+        static void RunOptions(Options o)
+        {
+            // Parser.Default.ParseArguments<Options>(args)
+            //         .WithParsed<Options>(o =>
+            //         {
+            //             opts = o;
+            //         });
+            opts = o;
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            
+        }
+        static void FormatFileName(string fileName)
+        {
+            foreach (char c in System.IO.Path.GetInvalidFileNameChars())
+            {
+                fileName = fileName.Replace(c, '_');
+            }
         }
     }
 }
--- options.cs
+++ options.cs
@@ -6,9 +6,21 @@ namespace canvas_downloader
     {
         [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
         public bool Verbose { get; set; }
+        
+        [Option('o', "output", Required = false, HelpText = "Specify output directory.")]
+        public string Output { get; set; }
+        
+[       Option('m', "nomodules", Required = false, HelpText = "Run program without downloading modules.")]
+        public bool NoModules { get; set; }
+        
+        [Option('f', "nofiles", Required = false, HelpText = "Run program without downloading files.")]
+        public bool NoFiles { get; set; }
 
-        [Option('n', "nocache", Required = false, HelpText = "Run the program with no cached data.")]
+        [Option('n', "nocache", Required = false, HelpText = "Run program without cached credentials.")]
         public bool NoCache { get; set; }
+        
+        [Option('x', "DeleteCache", Required = false, HelpText = "Deletes all cached login information.")]
+        public bool DeleteCache { get; set; }
         
     }
 }
